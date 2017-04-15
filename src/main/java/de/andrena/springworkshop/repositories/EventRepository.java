5696f01c18698d23bfa4d6e32ec1c88245986771
Christoph Kurrat <christoph.kurrat@andrena.de>
2017-04-15 14:59:29 +0200

    fix test, use types

--- src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
+++ src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
@@ -2,8 +2,20 @@ package de.andrena.springworkshop.repositories;
 
 import de.andrena.springworkshop.entities.Event;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RepositoryRestResource(path = "event")
 public interface EventRepository extends CrudRepository<Event, Integer> {
+    List<Event> findByTitleContaining(@Param("title") String title);
+
+    List<Event> findEventByDescriptionContaining(@Param("description") String description);
+
+    List<Event> findByStartTimeBefore(@Param("startTime") LocalDateTime startTime);
+
+    Long countAllByStartTimeBefore(@Param("startTime") LocalDateTime startTime);
+
 }
