60e6c24cf51818ddf1a0d8242108dd7de747f54c
Christoph Kurrat <christoph.kurrat@andrena.de>
2017-05-14 22:37:08 +0200

    fix react ui

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        client({method: 'GET', path: '/event'}).done(response => {
+        client({method: 'GET', path: '/events?projection=inlineSpeaker'}).done(response => {
             this.setState({events: response.entity._embedded.events});
     })
     }
