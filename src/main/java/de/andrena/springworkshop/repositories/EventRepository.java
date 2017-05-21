0b8889fc23090edf69a1a3bdee20f3102c07cb55
Christoph Kurrat <christoph.kurrat@andrena.de>
2017-05-22 00:56:55 +0200

    paging

--- src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
+++ src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
@@ -1,7 +1,9 @@
 package de.andrena.springworkshop.repositories;
 
 import de.andrena.springworkshop.entities.Event;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -12,9 +14,9 @@ import java.util.List;
 
 @CrossOrigin
 @RepositoryRestResource(path = "talks")
-public interface EventRepository extends CrudRepository<Event, Integer> {
+public interface EventRepository extends PagingAndSortingRepository<Event, Integer> { //CrudRepository<Event, Integer> {
 	@RestResource(path = "byTitle")
-	List<Event> findByTitleContaining(@Param("title") String title);
+	Page<Event> findByTitleContaining(@Param("title") String title, Pageable pageable);
 
 	List<Event> findEventByDescriptionContaining(@Param("description") String description);
 
