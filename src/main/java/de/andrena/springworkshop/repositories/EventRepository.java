8b715cc2dfd277107f021f622307f784f8fd7c1a
Christoph Kurrat <christoph.kurrat@andrena.de>
2017-05-21 16:48:10 +0200

    url mapping of some methods changed => show the benefits of HATEOAS

--- src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
+++ src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
@@ -4,14 +4,16 @@ import de.andrena.springworkshop.entities.Event;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @CrossOrigin
-@RepositoryRestResource
+@RepositoryRestResource(path = "talks")
 public interface EventRepository extends CrudRepository<Event, Integer> {
+	@RestResource(path = "byTitle")
 	List<Event> findByTitleContaining(@Param("title") String title);
 
 	List<Event> findEventByDescriptionContaining(@Param("description") String description);
