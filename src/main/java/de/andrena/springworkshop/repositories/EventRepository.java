fc8296b9c95fab42d194e5037b8503a9cba80693
Christoph Kurrat <christoph.kurrat@andrena.de>
2018-02-08 17:47:38 +0100

    cleanup

--- src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
+++ src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
@@ -2,21 +2,11 @@ package de.andrena.springworkshop.repositories;
 
 import de.andrena.springworkshop.entities.Event;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @CrossOrigin
 @RepositoryRestResource
 public interface EventRepository extends CrudRepository<Event, Integer> {
-	List<Event> findByTitleContaining(@Param("title") String title);
-
-	List<Event> findEventByDescriptionContaining(@Param("description") String description);
-
-	List<Event> findByStartTimeBefore(@Param("startTime") LocalDateTime startTime);
-
-	Long countAllByStartTimeBefore(@Param("startTime") LocalDateTime startTime);
+	//TODO implement search using event title
 }
