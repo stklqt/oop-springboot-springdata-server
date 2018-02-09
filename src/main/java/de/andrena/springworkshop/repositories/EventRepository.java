af2ace68604aa7f3745f0341fba9de8213ac53c4
Steffen Kleeberg <skleeberg@it-economics.de>
2018-02-09 10:53:37 +0100

    add search containing title

--- src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
+++ src/main/java/de/andrena/springworkshop/repositories/EventRepository.java
@@ -2,11 +2,17 @@ package de.andrena.springworkshop.repositories;
 
 import de.andrena.springworkshop.entities.Event;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @CrossOrigin
 @RepositoryRestResource
 public interface EventRepository extends CrudRepository<Event, Integer> {
 	//TODO implement search using event title
+
+    List<Event> findByTitleContaining(@Param(value = "title") String title);
+
 }
